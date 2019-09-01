5682f5ed63739e313cedf518b0f753c02d43164b
aditya007 <adityatandon007@gmail.com>
2019-09-01 19:52:53 +0530

    Updates

---
+++ precache-manifest.41a0d6c1b14121f72100ce3a31132886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d01cb9528532999a1f3474d7ed6a2f",
+    "url": "/tv-shows-scheduler/index.html"
+  },
+  {
+    "revision": "0251b68b7ccd6bb77d9f",
+    "url": "/tv-shows-scheduler/static/css/main.b53528ab.chunk.css"
+  },
+  {
+    "revision": "dd3f95fbc2a2da2ca437",
+    "url": "/tv-shows-scheduler/static/js/2.82f28157.chunk.js"
+  },
+  {
+    "revision": "0251b68b7ccd6bb77d9f",
+    "url": "/tv-shows-scheduler/static/js/main.b3b7881b.chunk.js"
+  },
+  {
+    "revision": "dae36c597d0cfa7a5299",
+    "url": "/tv-shows-scheduler/static/js/runtime~main.b9eca11a.js"
+  }
+]);
